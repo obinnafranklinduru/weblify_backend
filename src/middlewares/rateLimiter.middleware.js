caf061f356400cea2cf293727dd19708896765c4
Obinna Franklin Duru <obinnafranklinduru@gmail.com>
2023-10-12 23:29:10 +0100

    Update rateLimiter.middleware.js

--- src/middlewares/rateLimiter.middleware.js
+++ src/middlewares/rateLimiter.middleware.js
@@ -1,7 +1,7 @@
 const rateLimit = require('express-rate-limit');
 
 const limiter = rateLimit({
-	windowMs: 30 * 60 * 1000, // 15 minutes
+	windowMs: 45 * 60 * 1000, // 45 minutes
 	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
 	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
